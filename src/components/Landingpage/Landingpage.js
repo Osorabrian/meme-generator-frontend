e9fc24dce2855115067e157d87196a746e0b1636
Osorabrian <osorabrian@gmail.com>
2023-03-02 03:57:04 +0300

    create log in form component

--- src/components/Landingpage/Landingpage.js
+++ src/components/Landingpage/Landingpage.js
@@ -3,7 +3,7 @@ import './landingpage.css'
 
 export default function LandingPage(){
     return(
-        <div id="landing_page" style={{backgroundImage: "url('/landing_page.jpg')", height: "500px"}}>
+        <div id="landing_page" style={{backgroundImage: "url('/landing_page.jpg')", height: "540px"}}>
                 <h1>Meme Generator</h1>
         </div>
     )
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,11 +4,16 @@ import './login.css'
 export default function LogIn() {
     return(
         <form id="log_in">
-            <label className="form-label" htmlFor="email">Email</label>
-            <input type={"email"} className="form-control" placeholder="Email"/>
-            <label className="form-label" htmlFor="password">Password</label>
-            <input type={"password"} className="form-control" placeholder="Password"/>
-            <button type="submit">Log In</button>
+            <h2 style={{textAlign: "center"}}>Log In</h2>
+            <div className="mb-3">
+            <label className="form-label" htmlFor="email">Email:</label>
+            <input type={"email"} className="form-control" placeholder="Email" required/>
+            </div>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="password">Password:</label>
+                <input type={"password"} className="form-control" placeholder="Password" required/> 
+            </div>
+            <button type="submit" className="btn btn-primary">Log In</button>
         </form>
     )
 }
