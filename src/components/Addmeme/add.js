b35da32458131e852d630838022dcda4e54c61c2
Osorabrian <osorabrian@gmail.com>
2023-03-03 08:52:30 +0300

    change title of app

--- src/components/Addmeme/add.js
+++ src/components/Addmeme/add.js
@@ -1,24 +1,32 @@
 import React from "react";
+import './add.css'
 
 export default function AddMeme(){
     return(
-        <form>
-            <div className="mb-5">
-                <label className="form-label" htmlFor="title">Title:</label>
-                <input className="form-control" type={"text"} placeholder="Meme Title"/>
-            </div>
+        <div style={{backgroundImage: "url('/landing_page.jpg')", height: "542px"}}>
+           <div id="add-meme" className="text-right rounded" >
+               <h1 className="text-primary">Create Meme</h1>
+                <form >
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="title">Title:</label>
+                        <input className="form-control" type={"text"} placeholder="Meme Title"/>
+                    </div>
 
-            <div className="mb-5">
-                <label className="form-label" htmlFor="description">Description:</label>
-                <input className="form-control" type={"text"} placeholder="Enter Meme Description"/>
-            </div>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="description">Description:</label>
+                        <input className="form-control" type={"text"} placeholder="Enter Meme Description"/>
+                    </div>
 
-            <div className="mb-5">
-                <label className="form-label" htmlFor="meme_url">Meme URL:</label>
-                <input className="form-control" type={"text"} placeholder="Enter Meme URL"/>
-            </div>
-            
-            <button className="btn btn-primary" type="submit">Add Meme</button>
-        </form>
+                    <div className="mb-3">
+                        <label className="form-label" htmlFor="meme_url">Meme URL:</label>
+                        <input className="form-control" type={"text"} placeholder="Enter Meme URL"/>
+                    </div>
+                    
+                    <button className="btn btn-primary" type="submit">Add Meme</button>
+                </form>
+             </div>
+        </div>
+    
+        
     )
 }
